05e89b4f99fedb395dd81ea29ab05371aa2fa7cc
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-05-13 20:14:58 -0300

    Adicionado projeto lista de js

---
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/lista-exercicios-intro-js/exercicios.js
@@ -0,0 +1,141 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// // EXERCÍCIO 0A
+// function soma(num1, num2) {
+//   // implemente sua lógica aqui
+//   return num1 + num2
+// }
+
+// // EXERCÍCIO 0B
+// function imprimeMensagem() {
+//   // implemente sua lógica aqui
+//   const mensagem = prompt('Digite uma mensagem!')
+
+//   console.log(mensagem)
+// }
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// // EXERCÍCIO 01
+// function calculaAreaRetangulo(altura, largura) {
+//   // implemente sua lógica aqui
+//   altura = Number(prompt("Digite aqui a altura desejada"))
+//   largura = Number(prompt("Digite aqui a largura desejada"))
+//   const calculoAreaRetangulo = altura * largura
+//   console.log(calculoAreaRetangulo)
+// }
+
+// // EXERCÍCIO 02
+// function imprimeIdade(anoAtual, anoNascimento) {
+//   // implemente sua lógica aqui
+//   anoAtual = Number(prompt("Qual o ano em que estamos?"))
+//   anoNascimento = Number(prompt("Qual seu ano de nascimento?"))
+//   const calculoIdadeAtual = anoAtual - anoNascimento
+//   console.log(calculoIdadeAtual)
+
+// }
+
+// // EXERCÍCIO 03
+// function calculaIMC(peso, altura) {
+// //   // implemente sua lógica aqui
+// peso = Number(prompt("Qual o seu peso, em kg? (Utilize ponto no lugar da virgula"))
+// altura = Number(prompt("Qual a sua altura, em metros? (Utilize ponto no lugar da virgula"))
+// let  calculoIMC = peso / (altura * altura)
+// return calculoIMC
+// }
+
+// // // // EXERCÍCIO 04
+// function imprimeInformacoesUsuario(nome, idade, email) {
+// // //   // implemente sua lógica aqui
+// let = nome1 = nome = prompt("Qual o seu nome?")
+// let = idade1 = idade = Number(prompt("Qual a sua idade?"))
+// let = email1 = email = prompt("Qual o seu email?")
+
+// console.log(`Meu nome é ${nome1}, tenho ${idade1} anos, e o meu email é ${email1}.`)
+
+// // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+// }
+
+// // EXERCÍCIO 05
+// function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
+//   // implemente sua lógica aqui
+//  cor1 = prompt("Digite aqui uma cor favorita")
+//  cor2 = prompt("Digite aqui uma segunda cor favorita")
+//  cor3 = prompt("Digite aqui uma terceira cor favorita")
+// const coresFavoritas = [cor1, cor2, cor3]
+// console.log(coresFavoritas)
+// }
+
+// EXERCÍCIO 06
+// function retornaStringEmMaiuscula(string) {
+//   // implemente sua lógica aqui
+// let formataçãoMaiusculo = string.toUpperCase()
+// return formataçãoMaiusculo
+// }
+
+
+// // EXERCÍCIO 07
+// function calculaIngressosEspetaculo(custo, valorIngresso) {
+//   // implemente sua lógica aqui
+// let calculoQuantidadeSemPrejuizo = custo / valorIngresso
+// return calculoQuantidadeSemPrejuizo
+// }
+
+// // EXERCÍCIO 08
+// function checaStringsMesmoTamanho(string1, string2) {
+//   // implemente sua lógica aqui
+//   string1 = prompt("Coloque uma palavra")
+//   string2 = prompt("Coloque outra palavra")
+// let stringTamanho1 = string1.length
+// let stringTamanho2 = string2.length
+
+// let stringIguais = stringTamanho1 === stringTamanho2
+
+// return stringIguais
+// }
+
+// // EXERCÍCIO 09
+// function retornaPrimeiroElemento(array) {
+//   // implemente sua lógica aqui
+// let primeiroeElemento = array.shift()
+// return primeiroeElemento
+// }
+
+// EXERCÍCIO 10
+// function retornaUltimoElemento(array) {
+//   // implemente sua lógica aqui
+// let ultimoElemento = array.pop()
+// return ultimoElemento
+// }
+
+// // EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+//   // implemente sua lógica aqui
+
+
+// // EXERCÍCIO 12
+// function checaIgualdadeDesconsiderandoCase(string1, string2) {
+//   // implemente sua lógica aqui
+// const tamanho1 = string1.toLowerCase()
+// const tamanho2 = string2.t()
+// const trueOrFalse = tamanho1 === tamanho2
+// return trueOrFalse
+// }
+
+// EXERCÍCIO 13
+// function checaRenovacaoRG() {
+//   // implemente sua lógica aqui
+
+// }
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
---
+++ modulo1/projeto-lista-js/projeto-js/index.js
@@ -0,0 +1,16 @@
+/*const nome = "Patrick"
+const idade = "12"
+
+console.log("Olá","Meu nome é", nome, "e eu tenho", idade, "anos")*/
+
+/*const nome = "Patrick"
+const sobreNome = "Ferraz";
+const idade = 30;
+const souEstudante = true;*/
+
+//console.log("Olá", "meu nome é", nome, sobreNome,"e tenho", idade,"anos", souEstudante);
+
+/*const nome = prompt("Qual seu nome")
+const idade = prompt("Qual sua idade")
+
+console.log(typeof nome, typeof idade)*/
