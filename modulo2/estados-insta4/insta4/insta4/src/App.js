fec48235c9d85b31e964c4611d06b34ada025720
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-06-08 16:16:25 -0300

    Adicionado exercicio estados-insta4

---
+++ modulo2/estados-insta4/insta4/insta4/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={"paulinha"}
+          fotoUsuario={"https://picsum.photos/50/50"}
+          fotoPost={"https://picsum.photos/200/150"}
+        />
+
+        <Post
+          nomeUsuario={"patrick"}
+          fotoUsuario={"https://i.picsum.photos/id/1012/3973/2639.jpg?hmac=s2eybz51lnKy2ZHkE2wsgc6S81fVD1W2NKYOSh8bzDc"}
+          fotoPost={"https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"}
+        />
+
+        <Post
+          nomeUsuario={"sandra"}
+          fotoUsuario={"https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk"}
+          fotoPost={"https://i.picsum.photos/id/1019/5472/3648.jpg?hmac=2mFzeV1mPbDvR0WmuOWSiW61mf9DDEVPDL0RVvg1HPs"}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
