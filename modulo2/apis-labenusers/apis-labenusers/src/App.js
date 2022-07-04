4b040b128ff75103884eda37504a69bc4c507f05
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-07-04 13:50:09 -0300

    Exercício de use-state adicionado

---
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import axios from "axios";
+import TelaCadastro from "./components/TelaCadastro";
+import TelaListaUsuario from "./components/TelaListaUsuario";
+import styled from "styled-components";
+
+
+
+
+class App extends Component {
+  state = {
+    telaAtual: "cadastro"
+  }
+
+escolheTela = () => {
+  switch (this.state.telaAtual){
+    case "cadastro":
+      return  <TelaCadastro irParaLista={this.irParaLista}/>
+      case "lista":
+        return <TelaListaUsuario irParaCadastro={this.irParaCadastro}/>
+        default:
+          return <div>Pág não encontrada</div>
+  }
+}
+
+irParaCadastro = () =>{
+  this.setState({telaAtual: "cadastro"})
+}
+
+irParaLista= () =>{
+  this.setState({telaAtual: "lista"})
+}
+
+render(){
+  return(
+
+  <div>
+
+   {this.escolheTela()}
+    
+  </div>
+  )
+}
+}
+
+export default App;
---
+++ modulo3/use-state/insta-lab-hooks/src/components/Post/Post.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { PostContainer, PostHeader, UserPhoto, PostPhoto, PostFooter, CommenrtContaine } from './styles'
+
+import IconeComContador from '../IconeComContador/IconeComContador'
+import SecaoComentario from '../SecaoComentario/SecaoComentario'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+
+const Post = (props) => {
+
+  const [curtido, setCurtido] = useState(false);
+  const [numeroCurtidas, setNumeroCurtida] = useState(0);
+  const [comentando,setComentado] = useState(false);
+  const [numeroComentarios, setNumeroComentarios] = useState(0);
+  const [comentarios,setComentarios] = useState([])
+
+  const onClickCurtida = () => {
+    if(curtido){
+      setCurtido(!curtido);
+      setNumeroCurtida(numeroCurtidas - 1)
+    } else {
+      setCurtido(!curtido)
+      setNumeroCurtida(numeroCurtidas + 1)
+    }
+  };
+
+  const onClickComentario = () => {
+    setComentado(!comentando)
+  };
+
+  const enviarComentario = (comentario) => {
+    const listaDeComentarios = [...comentarios, comentario]
+      setComentarios(listaDeComentarios)
+      setComentado(false)
+      setNumeroComentarios(numeroComentarios + 1)
+    
+  }
+  
+  const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
+  const caixaDeComentario = comentando ? (
+    <SecaoComentario enviarComentario={enviarComentario}/>
+  ) : (
+    comentarios.map(comentario => {
+      return (
+        <CommenrtContaine>
+          <p>{comentario}</p>
+        </CommenrtContaine>
+      )
+    })
+  )
+
+  return (
+    <PostContainer>
+      <PostHeader>
+        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+         valorContador={numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+         valorContador={numeroComentarios}
+        />
+      </PostFooter>
+      {caixaDeComentario}
+    </PostContainer>
+  )
+}
+
+export default Post
