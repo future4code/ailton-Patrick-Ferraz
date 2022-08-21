15bcde952683b2b006654f9490e8b74d9dccbcfc
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-08-21 13:57:56 -0300

    minimos requesitos atendidos

--- modulo4/projeto-labebank/src/index.ts
+++ modulo4/projeto-labebank/src/index.ts
@@ -1,20 +1,37 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
-import { account, Users, BankStatement } from './data';
+import { account, Users} from './data';
 
 const app:Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/users",(req:Request, res:Response)=>{
-    res.status(200).send(account)
+app.get("/users/accounts",(req:Request, res:Response)=>{
+    try{
+        if(!account.length){
+            res.statusCode=404
+            throw new Error ("Nenhuma conta encontrada.")
+        }
+        res.status(200).send(account)
+    }catch(error:any){
+    res.send(error.message)
+    } 
 })
 
-app.post("/users" ,(req:Request,res: Response)=>{
+app.post("/users/create" ,(req:Request,res: Response)=>{
     let errorCode:number = 400;
     try{
         const {name,cpf,bithDate} = req.body
+        const [day, month, year] = bithDate.split("/")
+        const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
+        const ageInMilisseconds: number = Date.now() - dateOfBirth.getTime()
+
+        const ageInYears: number = ageInMilisseconds / 1000 / 60 / 60 / 24 / 365
+        if (ageInYears < 18){
+            res.statusCode = 406
+            throw new Error("Idade deve ser maior que 18 anos") 
+         }
 
         const newAccount:Users = {
             name: name,
